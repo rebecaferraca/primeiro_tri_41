63f5633b7bec8646133f77357ed0348db3cfa504
rebecaferraca <129127312+rebecaferraca@users.noreply.github.com>
2024-08-07 21:19:40 -0300

    ]ç

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function calculaTempo(tempoObjetivo) {
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
+    let dias = Math.floor(horas / 2);
 
     segundos %= 60;
     minutos %= 60;
